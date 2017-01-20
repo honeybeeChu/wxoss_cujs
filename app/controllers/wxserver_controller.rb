e3e1a87a90ce8c0433ff99f89decc889b7dfd129
chuliang <chuliang@asiainfo-sec.com>
2017-01-20 09:44:21 +0800

    增加对微信认证ｕｒｌ的ｈｔｔｐｇｅｔ的调用

--- app/controllers/wxserver_controller.rb
+++ app/controllers/wxserver_controller.rb
@@ -1,3 +1,4 @@
+require 'net/http'
 class WxserverController < ApplicationController
   skip_filter :authenticate, :url_check
 
@@ -6,31 +7,32 @@ class WxserverController < ApplicationController
     WORKER_LOG.info "用户点击上网的链接，进入该ｎｅｔａｕｔｈ方法"
     paramStr=params[:paramStr]
     WORKER_LOG.info "微信认证链接携带参数params为#{paramStr}"
-    responseURL = get(WXSERVER_NETAUTH + "?paramStr=" + paramStr)
 
-    WORKER_LOG.info "微信认证链接地址为#{responseURL}"
-    if responseURL.nil?
-      WORKER_LOG.info "微信认证报错，请查看相关日志"
-    else
-      redirect_to responseURL
+    getParamsStr = WXSERVER_NETAUTH + "?paramStr=" + paramStr
+
+    WORKER_LOG.info "http get方法的参数为#{getParamsStr}"
+
+    begin
+      responseURL = httpGet(getParamsStr)
+      WORKER_LOG.info "微信认证链接地址为#{responseURL}"
+
+      if responseURL.nil?
+        WORKER_LOG.info "微信认证报错，请查看相关日志"
+      else
+        redirect_to responseURL
+      end
+    rescue
+      WORKER_LOG.error "http get方法调用微信认证接口异常．"
     end
+
 
   end
 
-
   private
   # http get请求处理方法
-  def get(url)
-
-    uri = URI.parse(url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(uri.request_uri)
-
-    response = http.request(request)
-
+  def httpGet(url)
+    uri = URI(url)
+    Net::HTTP.get(uri)
   end
 
 end
